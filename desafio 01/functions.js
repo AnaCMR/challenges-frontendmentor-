4b309aee27f733c04300b0325e8020101594da29
Ana Marques Rodrigues <ana.silva@bandtec.com.br>
2021-12-27 19:04:28 -0300

    Finalização do desafio

--- desafio 01/functions.js
+++ desafio 01/functions.js
@@ -1,10 +1,17 @@
 const foto_produto = document.getElementById("foto_produto");
 
-function teste() {
+let block = false 
+function imagem_saber_mais() {
      hover_imagem.style.visibility = "visible";
-   setTimeout(function () {
-     hover_imagem.style.visibility = "hidden";
-   }, 500);
-console.log("teste")
+     }
+
+
+function imagem_inicial(){
+    setTimeout (function(){
+        hover_imagem.style.visibility = "hidden";
+    })
+
 }
-foto_produto.addEventListener('mouseenter', teste);
+
+foto_produto.addEventListener('mouseenter', imagem_saber_mais);
+foto_produto.addEventListener('mouseleave',imagem_inicial);
