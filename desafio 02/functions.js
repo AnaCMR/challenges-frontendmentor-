0834dfeacfbc65bba3a617c48955c0aeccca2544
Ana Marques Rodrigues <ana.silva@bandtec.com.br>
2021-12-28 01:00:10 -0300

    Inicio js

---
+++ desafio 02/functions.js
@@ -0,0 +1,73 @@
+function diario (){
+    diario_id.style.color = "hsl(236, 100%, 87%)";
+    semanal_id.style.color = "hsl(235, 45%, 61%)";
+    mensal_id.style.color = "hsl(235, 45%, 61%)";
+    trabalho_dia.style.display = "block";
+    trabalho_semana.style.display = "none";
+    trabalho_mes.style.display = "none";
+    social_dia.style.display = "block";
+    social_semana.style.display = "none";
+    social_mes.style.display = "none";
+    cuidado_dia.style.display = "block";
+    cuidado_semana.style.display = "none";
+    cuidado_mes.style.display = "none";
+    jogo_dia.style.display = "block";
+    jogo_semana.style.display = "none";
+    jogo_mes.style.display = "none";
+    exercicio_dia.style.display = "block";
+    exercicio_semana.style.display = "none";
+    exercicio_mes.style.display = "none";
+    estudo_dia.style.display = "block";
+    estudo_semana.style.display = "none";
+    estudo_mes.style.display = "none";
+
+}
+
+function semanal(){
+    diario_id.style.color = "hsl(235, 45%, 61%)";
+    semanal_id.style.color = "hsl(236, 100%, 87%)";
+    mensal_id.style.color = "hsl(235, 45%, 61%)";
+    
+    trabalho_dia.style.display = "none";
+    trabalho_semana.style.display = "block";
+    trabalho_mes.style.display = "none";
+    social_dia.style.display = "none";
+    social_semana.style.display = "block";
+    social_mes.style.display = "none";
+    cuidado_dia.style.display = "none";
+    cuidado_semana.style.display = "block";
+    cuidado_mes.style.display = "none";
+    jogo_dia.style.display = "none";
+    jogo_semana.style.display = "block";
+    jogo_mes.style.display = "none";
+    exercicio_dia.style.display = "none";
+    exercicio_semana.style.display = "block";
+    exercicio_mes.style.display = "none";
+    estudo_dia.style.display = "none";
+    estudo_semana.style.display = "block";
+    estudo_mes.style.display = "none";
+}
+
+function mensal(){
+    diario_id.style.color = "hsl(235, 45%, 61%)";
+    semanal_id.style.color = "hsl(235, 45%, 61%)";
+    mensal_id.style.color = "hsl(236, 100%, 87%)";
+    trabalho_dia.style.display = "none";
+    trabalho_semana.style.display = "none";
+    trabalho_mes.style.display = "block";
+    social_dia.style.display = "none";
+    social_semana.style.display = "none";
+    social_mes.style.display = "block";
+    cuidado_dia.style.display = "none";
+    cuidado_semana.style.display = "none";
+    cuidado_mes.style.display = "block";
+    jogo_dia.style.display = "none";
+    jogo_semana.style.display = "none";
+    jogo_mes.style.display = "block";
+    exercicio_dia.style.display = "none";
+    exercicio_semana.style.display = "none";
+    exercicio_mes.style.display = "block";
+    estudo_dia.style.display = "none";
+    estudo_semana.style.display = "none";
+    estudo_mes.style.display = "block";
+}
