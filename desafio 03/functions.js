14887e84f3cb8a47e053a8df068b3862f6e41b12
Ana Marques Rodrigues <ana.silva@bandtec.com.br>
2021-12-31 15:55:12 -0300

    Js 80%

--- desafio 03/functions.js
+++ desafio 03/functions.js
@@ -1,91 +1,108 @@
 const tipSelected = document.querySelectorAll(".tips-selection li");
-const buttonResult = document.querySelectorAll(".result button")
+const buttonResult = document.querySelectorAll(".result button");
 // console.log(buttonSelected)
-let tipItens = ""
-let tipArrey = []
-let resultTipPerson = 0
-let resultTip = 0
-let totalResult = 0
+let tipItens = "";
+let tipArrey = [];
+let resultTipPerson = 0;
+let resultTip = 0;
+let totalResult = 0;
+let teste = 0
 
 function tipValue(index) {
-    let customTip = customInput.value;
-    let billValue = Number(billInput.value)
-    let numberPeople = Number(personInput.value);
-    tipSelected.forEach((actived) => {
-       tipItens += actived.innerHTML;
-       tipArrey = tipItens.split("%")
-       tipArrey.pop()
-       tipArrey.push(Number(customTip))
-       resultTip = ((tipArrey[index]/100)*billValue)
-       resultTipPerson = resultTip/ numberPeople
+  let customTip = customInput.value;
+  let billValue = Number(billInput.value);
+  tipSelected.forEach((actived) => {
+    tipItens += actived.innerHTML;
+    tipArrey = tipItens.split("%");
+    tipArrey.pop();
+    tipArrey.push(customTip);
+    resultTip = (tipArrey[index] / 100);
+    teste = resultTip * billValue
+
+    console.log(tipArrey[index])
   });
 }
 
 function activeTip(index) {
-    tipSelected.forEach((actived) => {
-        actived.classList.remove("button-selected");
-    });
-    tipSelected[index].classList.add('button-selected')
+  tipSelected.forEach((actived) => {
+    actived.classList.remove("button-selected");
+  });
+  tipSelected[index].classList.add("button-selected");
 }
 
 tipSelected.forEach((buttonActive, index) => {
-    buttonActive.addEventListener("click", () => {
-    activeTip(index)
-    tipValue(index)
-    });
+  buttonActive.addEventListener("click", () => {
+    activeTip(index);
+    tipValue(index);
+  });
 });
 
 function alterTotals() {
-    let billValue = billInput.value;
-    let numberPeople = personInput.value;
-    totalResult = resultTipPerson + (billValue/numberPeople);
+  let billValue = Number(billInput.value);
+  let numberPeople = Number(personInput.value);
+  resultTipPerson = teste / numberPeople;
+  totalResult = resultTipPerson + (billValue / numberPeople);
 
-    if (billValue !== 0 && numberPeople !== 0){
-        totalTip.innerHTML = resultTipPerson.toFixed(2)
-        total.innerHTML = totalResult.toFixed(2)
-    }
-
+  if (billValue != 0 && numberPeople != 0) {
+    totalTip.innerHTML = resultTipPerson.toFixed(2);
+    total.innerHTML = totalResult.toFixed(2);
+  }
+  resetButton()
 }
 
-
-const insertInfo = document.querySelectorAll ('.empty')
-const errorInputBill = document.querySelectorAll('#billInput')
+const insertInfo = document.querySelectorAll(".empty");
+const errorInputBill = document.querySelectorAll("#billInput");
 function confirmInfoBill() {
-    let billValue = Number(billInput.value)
-    insertInfo.forEach((changeStatus) => {
-    changeStatus.classList.remove('empty')
-})
-if(billValue === 0){
+  let billValue = Number(billInput.value);
+  insertInfo.forEach((changeStatus) => {
+    changeStatus.classList.remove("empty");
+  });
+  if (billValue === 0) {
     errorInputBill.forEach((showError) => {
-        errorBill.style.visibility = "visible"
-        showError.classList.add("error-input")
-        showError.classList.remove("empty")
+      errorBill.style.visibility = "visible";
+      showError.classList.add("error-input");
+      showError.classList.remove("empty");
     });
-} else {
+  } else {
     errorInputBill.forEach((showError) => {
-        errorBill.style.visibility = "hidden"
-        showError.classList.remove("error-input")
-});
+      errorBill.style.visibility = "hidden";
+      showError.classList.remove("error-input");
+    });
+  }
 }
-} 
 
-const errorInputPerson = document.querySelectorAll('#personInput')
+const errorInputPerson = document.querySelectorAll("#personInput");
 function confirmInfoPerson() {
-    let numberPeople = Number(personInput.value);
-    insertInfo.forEach((changeStatus) => {
-    changeStatus.classList.remove('empty')
-})
-if(numberPeople === 0){
+  let numberPeople = Number(personInput.value);
+  insertInfo.forEach((changeStatus) => {
+    changeStatus.classList.remove("empty");
+  });
+  if (numberPeople === 0) {
     errorInputPerson.forEach((showError) => {
-        errorPeople.style.visibility = "visible"
-        showError.classList.add("error-input")
-        showError.classList.remove("empty")
+      errorPeople.style.visibility = "visible";
+      showError.classList.add("error-input");
+      showError.classList.remove("empty");
     });
-} else {
+  } else {
     errorInputPerson.forEach((showError) => {
-        errorPeople.style.visibility = "hidden"
-        showError.classList.remove("error-input")
-        alterTotals()
-});
+      errorPeople.style.visibility = "hidden";
+      showError.classList.remove("error-input");
+      alterTotals();
+    });
+  }
 }
+
+const buttonReset = document.querySelectorAll(".empty-button")
+
+function resetButton(){
+
+  if(totalTip.innerHTML != 0 && total.innerHTML != 0){
+    buttonReset.forEach((changeStatus) => {
+      changeStatus.classList.add("result-button");
+      changeStatus.classList.remove("empty-button")
+      changeStatus.addEventListener('click', () => {
+        location.reload()
+      })
+    })
+  }
 }
