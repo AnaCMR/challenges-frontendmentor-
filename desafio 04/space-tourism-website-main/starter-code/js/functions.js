19093c765df625d4b03383615801642be5583886
Ana Marques Rodrigues <ana.silva@bandtec.com.br>
2022-01-07 18:47:54 -0300

    finalização da index

---
+++ desafio 04/space-tourism-website-main/starter-code/js/functions.js
@@ -0,0 +1,23 @@
+// const pages = document.querySelectorAll('a')
+
+// function handleClick(event){
+//     event.preventDefault();
+//     fetchPage(event.target.href)
+// }
+
+// async function fetchPage(url){
+//     const pageResponse = await fetch(url);
+//     const pageText = await pageResponse.text();
+//     replaceContent(pageText)
+// }
+
+// function replaceContent(newText){
+//     const newHtml = document.createElement('div');
+//     nreHtml.innerHTML = newText;
+
+//     const oldContent = document.querySelector('.')
+// }
+
+// pages.forEach(link) => {
+//     pages.addEventListener('click',handleClick)
+// }
